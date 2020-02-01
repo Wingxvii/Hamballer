debbe93962d4d00cbf23759f08bc611e17f060c3
John <john.wang@uoit.net>
2020-02-01 13:04:53 -0500

    improved hamster jump

--- Assets/_Scripts/HamsterMovement.cs
+++ Assets/_Scripts/HamsterMovement.cs
@@ -8,7 +8,7 @@ public class HamsterMovement : MonoBehaviour
     public float Speed;
     public float Jump;
     public Rigidbody2D hamsterRigid;
-
+    public Transform hamsterBall;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +21,7 @@ public class HamsterMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
             //Jump
-            hamsterRigid.velocity = new Vector2(hamsterRigid.velocity.x, hamsterRigid.velocity.y + Jump);
+            hamsterRigid.velocity += new Vector2(hamsterBall.position.x - transform.position.x, hamsterBall.position.y - transform.position.y) * Jump;
         }
     }
 
