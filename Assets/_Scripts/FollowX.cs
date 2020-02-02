190ec0ad3d04d6331e51462304108b1e06fadf4e
John <john.wang@uoit.net>
2020-02-02 12:31:55 -0500

    added minimap

---
+++ Assets/_Scripts/FollowX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowX : MonoBehaviour
+{
+
+    public Transform target;
+    public float highFollow;
+
+    float defualtY;
+
+    private void Start()
+    {
+        defualtY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yValue = defualtY;
+        if (target.position.y > highFollow) {
+            yValue = defualtY + (target.position.y - highFollow);
+        }
+
+        transform.position = new Vector3(target.position.x, yValue, -100);
+
+    }
+}
